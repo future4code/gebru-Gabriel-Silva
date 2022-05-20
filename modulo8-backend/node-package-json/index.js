b5ab81cc703fe18c8d5ccebf95832c79fd9e9a7d
gabriel <dgabridb3@gmail.com>
2022-05-19 22:07:19 -0300

    exercicio node

---
+++ modulo8-backend/node-package-json/index.js
@@ -0,0 +1,16 @@
+//usando console.log
+/* const nome = process.argv[2]
+const idade = Number(process.argv[3])
+const idadeDaquiSeteAnos = Number(process.argv[4])
+console.log(`Olá, ${nome}! Você tem ${idade} anos.`)
+console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${idadeDaquiSeteAnos}`)
+
+const numeroUm = Number(process.argv[5])
+const numeroDois = Number(process.argv[6])
+const resultado = numeroDois + numeroUm
+console.log(`Resultado: ${resultado}`) */
+
+const tarefas = [
+
+]
+console.log (tarefas)
