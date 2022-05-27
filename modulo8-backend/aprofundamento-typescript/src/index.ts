57128b24d76055ca4dd28999aff5f010f88247df
Gabriel <dgabridb3@gmail.com>
2022-05-26 21:55:05 -0300

    exercicio de type

---
+++ modulo8-backend/aprofundamento-typescript/src/index.ts
@@ -0,0 +1,83 @@
+// A 
+//let minhaString: string = 2
+// Aparece um erro dizendo que não pode atribuir string a number
+
+// B 
+//let meuNumero:number = ""
+// Aparece um erro dizendo que não pode atribuir número a string
+
+
+
+/* enum corFavorita{
+    violeta = "violeta",
+    anil = "anil",
+    azul = "azul",
+    verde = "verde",
+    amarelo = "amarelo"
+
+}
+
+type person = { nome: string, idade: number, corFavorita: string}
+
+
+
+const person: person = {
+    nome: "Astrodev",
+    idade: 30,
+    corFavorita: corFavorita.amarelo
+    }
+ */
+
+  /*   function obterEstatisticas(numeros:number[]) {
+
+        const numerosOrdenados = numeros.sort(
+            (a:number, b:number) => a - b
+        )
+    
+        let soma:number = 0
+    
+        for (let num of numeros) {
+            soma += num
+        }
+    
+        const estatisticas = {
+            maior: numerosOrdenados[numeros.length - 1],
+            menor: numerosOrdenados[0],
+            media: soma / numeros.length
+        }
+    
+        return estatisticas
+    }
+    type amostraDeIdades = {
+
+		numeros: [21, 18, 65, 44, 15, 18],
+
+		obterEstatisticas: (numeros:number[]) => void
+} */
+
+/* type post = { autor: string, texto: string}
+type Users = post[] 
+
+const posts = [
+    {
+      autor: "Alvo Dumbledore",
+      texto: "Não vale a pena viver sonhando e se esquecer de viver"
+    },
+    {
+      autor: "Severo Snape",
+      texto: "Menos 10 pontos para Grifinória!"
+    },
+    {
+      autor: "Hermione Granger",
+      texto: "É levi-ô-sa, não levio-sá!"
+    },
+    {
+      autor: "Dobby",
+      texto: "Dobby é um elfo livre!"
+    },
+    {
+      autor: "Lord Voldemort",
+      texto: "Avada Kedavra!"
+    }
+  ] */
+
