0b57da8fa90f4d905a7fb1721dcd9f2debcc1155
Gabriel <dgabridb3@gmail.com>
2022-05-31 21:54:08 -0300

    exercicio intro express

---
+++ modulo8-backend/intro-express/src/post.ts
@@ -0,0 +1,92 @@
+export const post = [
+    {
+      "id": 1,
+      "nome": "Leanne Graham",
+      "username": "Bret",
+      "email": "Sincero@april.biz",
+      "telefone": "1-770-736-8031 x56442",
+      "website": "hildegard.org",
+
+    },
+    {
+      "id": 2,
+      "nome": "Ervin Howell",
+      "username": "Antonette",
+      "email": "Shanna@melissa.tv",
+      "telefone": "010-692-6593 x09125",
+      "site": "anastasia.net",
+    },
+    {
+      "id": 3,
+      "name": "Clementine Bauch",
+      "username": "Samantha",
+      "email": "Nathan@yesenia.net",
+      "telefone": "1-463-123-4447",
+      "site": "ramiro.info",
+    },
+    {
+      "id": 4,
+      "nome": "Patrícia Lebsack",
+      "username": "Karianne",
+      "email": "Julianne.OConner@kory.org",
+    
+      "telefone": "493-170-9623 x156",
+      "site": "kale.biz",
+     
+    },
+    {
+      "id": 5,
+      "nome": "Chelsey Dietrich",
+      "username": "Kamren",
+      "email": "Lucio_Hettinger@annie.ca",
+    
+      "telefone": "(254)954-1289",
+      "site": "demarco.info",
+     
+    },
+    {
+      "id": 6,
+      "name": "Sra. Dennis Schulist",
+      "username": "Leopoldo_Corkery",
+      "email": "Karley_Dach@jasper.info",
+     
+      "telefone": "1-477-935-8478 x6430",
+      "site": "ola.org",
+    },
+    {
+      "id": 7,
+      "nome": "Kurtis Weissnat",
+      "username": "Elwyn.Skiles",
+      "email": "Telly.Hoeger@billy.biz",
+    
+      "telefone": "210.067.6132",
+      "site": "elvis.io",
+    },
+    {
+      "id": 8,
+      "name": "Nicholas Runolfsdottir V",
+      "username": "Maxime_Nienow",
+      "email": "Sherwood@rosamond.me",
+    
+      "telefone": "586.493.6943 x140",
+      "site": "jacynthe.com",
+    },
+    {
+      "id": 9,
+      "nome": "Glenna Reichert",
+      "username": "Delphine",
+      "email": "Chaim_McDermott@dana.io",
+      "telefone": "(775)976-6794 x41206",
+      "site": "conrad.com",
+    
+    },
+    {
+      "id": 10,
+      "nome": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "email": "Rey.Padberg@karina.biz",
+      "telefone": "024-648-3804",
+      "site": "ambrose.net",
+     
+    }
+  ]
